037a7ea6d69184b1864abd7c499dbb79c413c623
llogith88 <llogith09@gmail.com>
2026-09-12 20:57:33 +0530

    Store Changes

---
+++ src/week6_assignments/EmployeeCompanyInformationManagement.java
@@ -0,0 +1,47 @@
+package week6_assignments;
+
+
+
+public class EmployeeCompanyInformationManagement {
+
+    static class Employee {
+
+        String empName;
+        double salary;
+
+        static String companyName =
+                "Bright Horizon Technologies";
+
+        static int employeeCount = 0;
+
+        Employee(String empName, double salary) {
+
+            this.empName = empName;
+            this.salary = salary;
+
+            employeeCount++;
+        }
+
+        static void printCompanyInfo() {
+
+            System.out.println(companyName);
+            System.out.println(
+                    "Employees on record: " + employeeCount
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee employee1 =
+                new Employee("Divya", 65000);
+
+        Employee employee2 =
+                new Employee("Arjun", 40000);
+
+        Employee employee3 =
+                new Employee("Rahul", 55000);
+
+        Employee.printCompanyInfo();
+    }
+}
